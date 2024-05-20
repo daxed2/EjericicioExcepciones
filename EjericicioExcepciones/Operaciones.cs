f24957fca1511631590e78af3685ebc61e40fba2
daxed2 <158123332+daxed2@users.noreply.github.com>
2024-05-19 21:19:33 -0600

    Agregar archivos de proyecto.

---
+++ EjericicioExcepciones/Operaciones.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjericicioExcepciones
+{
+    public class Operaciones
+    {
+        public double Sumar(double num1, double num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Dividir(double num1, double num2)
+        {
+            return num1 / num2;
+        }
+
+        public double Restar(double num1, double num2)
+        {
+            return num1 - num2;
+        }
+
+        public double Multiplicar(double num1, double num2)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ EjericicioExcepciones/Program.cs
@@ -0,0 +1,60 @@
+﻿using EjericicioExcepciones;
+
+string opcion = "";
+Operaciones operaciones = new Operaciones();
+
+try
+{
+    do
+    {
+        Console.WriteLine("Ingrese el tipo de operación a realizar");
+        Console.WriteLine("1. Sumar \n2. Restar \n3. Dividir \n4. Multiplicar");
+        string tipoOperacion = Console.ReadLine();
+        Console.WriteLine("Ingrese el num1");
+        string num1 = Console.ReadLine();
+        Console.WriteLine("Ingrese el num2");
+        string num2 = Console.ReadLine();
+        controladorOperaciones(num1, num2, tipoOperacion, operaciones);
+
+        Console.WriteLine("Desea continuar? (S/N)");
+        opcion = Console.ReadLine();
+    } while (opcion != "N");
+}
+catch(Exception e)
+{
+    Console.WriteLine($"Error: {e.Message}");
+}
+
+
+
+void controladorOperaciones(string num1, string num2,string tipoOperacion,Operaciones operaciones)
+{
+    //Validaciones
+    double n1, n2,resultado;
+
+    if (!double.TryParse(num1, out n1))
+        throw new Exception("El dato de entrada del número 1 debe ser númerico");
+
+    if (!double.TryParse(num2, out n2))
+        throw new Exception("El dato de entrada del número 2 debe ser númerico");
+
+    switch (tipoOperacion)
+    {
+        case "1":
+            resultado = operaciones.Sumar(n1, n2);
+            break;
+        case "2":
+            resultado = operaciones.Restar(n1, n2);
+            break;
+        case "3":
+            resultado = operaciones.Dividir(n1, n2);
+            break;
+        case "4":
+            resultado = operaciones.Multiplicar(n1, n2);
+            break;
+        default:
+            throw new Exception("Opción no válida para el tipo de operación. Por favor, seleccione una opción válida.");
+           
+    }
+    Console.WriteLine($"El resultado es {resultado}");
+}
